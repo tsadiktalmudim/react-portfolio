69044d81d6385dd18119033e5cbda2b2610da82c
Xan <tsadiktalmudim@gmail.com>
2022-08-25 00:32:39 -0500

    refined project page scalability and added descriptions

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { Grid, Button } from "@mui/material";
+import { Grid } from "@mui/material";
 import crescendo from "../assets/images/Crescendo.png";
-import gitItDone from "../assets/images/git-it-done.png";
-import runBuddy from "../assets/images/run-buddy.png";
 import voyager from "../assets/images/voyager.png";
 import passwordGenerator from "../assets/images/password-generaor.png";
 import weatherDashboard from "../assets/images/weather-dashboard.png";
@@ -10,61 +8,93 @@ import babelfish from "../assets/images/babelfish.png";
 
 export default function Projects() {
   return (
-    <Grid container spacing={5}>
-      <Grid className="container" item xs={10} margin="5px">
+    <Grid container alignContent="center" alignItems="center">
+      <Grid xs={1}></Grid>
+      <Grid className="container large" item xs={8} margin="5px">
         <a href="https://babelfishapp.herokuapp.com/">
-          <img
-            src={babelfish}
-            width={1100}
-            height-={550}
-            alt="babelfish app"
-          ></img>
-          <div className="info-lg">BabelFish Translator Chat App</div>
+          <img src={babelfish} alt="babelfish app"></img>
+          <div className="proname-lg">BabelFish Translator Chat App</div>
         </a>
       </Grid>
-      <Grid item className="container" xs={6}>
+      <Grid xs={2}>
+        <p>
+          Chat app that translates in real time between an unlimited amount of
+          users
+        </p>
+        <p>Role: Collaborator (Front and back end)</p>
+        <p>
+          Tools: React, JSX, CSS, Material UI, Javascript, GoogleFonts, Node,
+          Socket.IO, Express, MongoDB, Google Translate, JWT, Heroku, JawsDB,
+          etc.
+        </p>
+        <p><a href="https://github.com/jrj-sys/BabelFish">GitHub Repo</a></p>
+      </Grid>
+      <Grid xs={1}></Grid>
+      <Grid item className="container small" xs={3}>
+        <img src={voyager} alt="voyager app"></img>
         <a href="https://tsadiktalmudim.github.io/Project-1-Team-Voyager/">
-          <img
-            src={voyager}
-            width={600}
-            height-={300}
-            alt="voyager app"
-          ></img>
-          <div className="info">Voyager Space API</div>
+          <div className="proname">Voyager Space API</div>
         </a>
-      </Grid>{" "}
-      <Grid item className="container" xs={6}>
+      </Grid>
+      <Grid xs={2}>
+        <p>
+          Space tracker website using a REST API with current space data
+          including launches, astronauts, etc.
+        </p>
+        <p>Role: Collaborator (Front and back end)</p>
+        <p>
+          Tools: HTML, CSS, JS, W3CSS, GoogleFonts, jQuery, jQueryUI, MomentJS
+        </p>
+        <p><a href="https://github.com/tsadiktalmudim/Project-1-Team-Voyager">GitHub Repo</a></p>
+      </Grid>
+      <Grid item className="container small" xs={3}>
+        <img src={crescendo} alt="crescendo app"></img>
         <a href="https://obscure-brook-13370.herokuapp.com/">
-          <img
-            src={crescendo}
-            width={550}
-            height={300}
-            alt="crescendo app"
-          ></img>
-          <div className="info">Crescendo Music Sharing App</div>
+          <div className="proname">Crescendo Music Sharing App</div>
         </a>
       </Grid>
-      <Grid item className="container" xs={6}>
+      <Grid xs={2}>
+        <p>
+          Space tracker website using a REST API with current space data
+          including launches, astronauts, etc.
+        </p>
+        <p>Role: Collaborator (Front and back end)</p>
+        <p>
+          Tools: HTML, CSS, JS, W3CSS, GoogleFonts, jQuery, jQueryUI, MomentJS
+        </p>
+        <p><a href="https://github.com/laynewegenast/proj-2-music-site">GitHub Repo</a></p>
+      </Grid>
+      <Grid xs={1}></Grid>
+      <Grid xs={1}></Grid>
+      <Grid item className="container small" xs={3}>
+        <img src={weatherDashboard} alt="babelfish app"></img>
         <a href="https://tsadiktalmudim.github.io/weather-dashboard/">
-          <img
-            src={weatherDashboard}
-            width={600}
-            height-={300}
-            alt="babelfish app"
-          ></img>
-          <div className="info">Weather Dashboard</div>
+          <div className="proname">Weather Dashboard</div>
         </a>
       </Grid>
-      <Grid item className="container" xs={6}>
+      <Grid xs={2}>
+        <p>
+          Weather app that allows you to search a city and receive its 5-day
+          forecast
+        </p>
+        <p>Role: Collaborator (Front and back end)</p>
+        <p>Tools: HTML, CSS, W3CSS, JS, jQuery, OpenWeather API, locaStorage</p>
+        <p><a href="https://github.com/tsadiktalmudim/weather-dashboard">GitHub Repo</a></p>
+      </Grid>
+      <Grid item className="container small" xs={3}>
+        <img src={passwordGenerator} alt="babelfish app"></img>
         <a href="https://tsadiktalmudim.github.io/password-generator/">
-          <img
-            src={passwordGenerator}
-            width={600}
-            height-={300}
-            alt="babelfish app"
-          ></img>
-          <div className="info">Random Password Generator</div>
+          <div className="proname">Random Password Generator</div>
         </a>
+      </Grid>
+      <Grid xs={2}>
+        <p>
+          Random password generator that allows you to choose length and
+          character type for a randomly generated password
+        </p>
+        <p>Role: Sole Author</p>
+        <p>Tools: HTML, CSS, JS</p>
+        <p><a href="https://github.com/tsadiktalmudim/password-generator">GitHub Repo</a></p>
       </Grid>
     </Grid>
   );
